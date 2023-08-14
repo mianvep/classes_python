3b970c470f5cdc7b271c18da7b62dd74c9a6e766
Miguel Vera <miguelverap01@gmail.com>
2023-08-13 22:42:07 -0500

    clase uno de python

---
+++ main.py
@@ -0,0 +1,8 @@
+hours_woks = float(input("Cuantas horas trabajas al día: "))
+
+salary = float(input("Cuanto ganas por hora: "))
+
+salary_moth = hours_woks * salary * 30
+
+print("Tu salario mensual es: ", salary_moth)
+
